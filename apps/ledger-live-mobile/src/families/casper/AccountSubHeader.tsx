df7061021adc99877cdbc086569b81a5c3c93945
Emmanuel <emmanuelm41@gmail.com>
2022-11-02 10:13:07 -0300

    add headers on mobile and desktop

---
+++ apps/ledger-live-mobile/src/families/casper/AccountSubHeader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AccountSubHeader from "../../components/AccountSubHeader";
+
+function FilecoinAccountSubHeader() {
+  return <AccountSubHeader family="Casper" team="Zondax" />;
+}
+
+export default FilecoinAccountSubHeader;
