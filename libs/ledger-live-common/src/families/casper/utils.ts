d8ad27825bd4b1900ced4486e822aef6d09614d9
Prateek Rathod <prateek@zondax.ch>
2022-11-03 15:09:08 +0530

    fix: sync for ledger cli

--- libs/ledger-live-common/src/families/casper/utils.ts
+++ libs/ledger-live-common/src/families/casper/utils.ts
@@ -1,4 +1,3 @@
-import { BigNumber } from "bignumber.js";
 import { flatMap } from "lodash";
 import { encodeAccountId } from "../../account";
 import { GetAccountShape } from "../../bridge/jsHelpers";
@@ -7,7 +6,10 @@ import {
   fetchBalances,
   fetchBlockHeight,
   fetchTxs,
+  getAccountStateInfo,
 } from "./bridge/utils/network";
+import { log } from "@ledgerhq/logs";
+import BigNumber from "bignumber.js";
 
 const validHexRegExp = new RegExp(/[0-9A-Fa-f]{6}/g);
 const validBase64RegExp = new RegExp(
@@ -64,17 +66,40 @@ export const getAccountShape: GetAccountShape = async (info) => {
     derivationMode,
   });
 
-  const blockHeight = await fetchBlockHeight();
-  const balance = await fetchBalances(address.substring(2));
-  const txs = await fetchTxs(address);
+  log("debug", `Generation account shape for ${address}`);
 
+  const { purseUref, accountHash } = await getAccountStateInfo(address);
+
+  const blockHeight = await fetchBlockHeight();
+
+  let balance, txs;
+  if (purseUref && accountHash) {
+    balance = await fetchBalances(address, purseUref);
+    txs = await fetchTxs(accountHash);
+  } else {
+    balance = 0;
+    txs = [];
+  }
+
+  const csprBalance = motesToCSPR(balance.balance_value);
   const result = {
     id: accountId,
-    balance: new BigNumber(balance.balance_value),
-    spendableBalance: new BigNumber(balance.balance_value),
+    balance: csprBalance,
+    spendableBalance: csprBalance,
     operations: flatMap(txs, mapTxToOps(accountId, info)),
     blockHeight: blockHeight.last_added_block_info.height,
   };
 
   return result;
 };
+
+export function motesToCSPR(motes: number | string): BigNumber {
+  if (!motes) return new BigNumber(0);
+
+  return new BigNumber(motes).div(1000000000);
+}
+
+export function csprToMotes(cspr: number | string): BigNumber {
+  if (!cspr) return new BigNumber(0);
+  return new BigNumber(cspr).multipliedBy(1000000000);
+}
