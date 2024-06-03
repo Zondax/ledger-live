548cb7e91ca42bb49e491dc212ed20dc8bdfecd9
rllola <rllola80@gmail.com>
2024-06-03 17:13:34 +0200

    tests are working

--- libs/coin-modules/coin-kadena/src/bridge.integration.test.ts
+++ libs/coin-modules/coin-kadena/src/bridge.integration.test.ts
@@ -23,20 +23,17 @@ const kadena: CurrenciesData<Transaction> = {
   ],
   accounts: [
     {
+      FIXME_tests: ["balance is sum of ops"],
       raw: {
         id: `js:2:kadena:15daab7d9ba9f8a465ffc4bfb33e68ca5e9f51ef0bab387284963129fe04ec3e:`,
         seedIdentifier: PUBKEY,
         name: "Kadena 1",
         derivationMode: "",
         index: 0,
+        xpub: "15daab7d9ba9f8a465ffc4bfb33e68ca5e9f51ef0bab387284963129fe04ec3e",
         freshAddress: "k:15daab7d9ba9f8a465ffc4bfb33e68ca5e9f51ef0bab387284963129fe04ec3e",
         freshAddressPath: "44'/626'/0'/0/0'",
-        freshAddresses: [
-          {
-            address: "k:15daab7d9ba9f8a465ffc4bfb33e68ca5e9f51ef0bab387284963129fe04ec3e",
-            derivationPath: "44'/626'/0'/0/0"
-          }
-        ],
+        freshAddresses: [],
         blockHeight: 4333588,
         operations: [],
         pendingOperations: [],
@@ -52,7 +49,7 @@ const kadena: CurrenciesData<Transaction> = {
           transaction: t => ({
             ...t,
             amount: new BigNumber(20),
-            recipient: "k:77b021744ab3c003e8e4d0f38a598f0e39fe9a7fe61360754dc7321b112ab375",
+            recipient: "k:15daab7d9ba9f8a465ffc4bfb33e68ca5e9f51ef0bab387284963129fe04ec3e",
           }),
           expectedStatus: {
             errors: {
@@ -66,15 +63,16 @@ const kadena: CurrenciesData<Transaction> = {
   ]
 };
 
+describe("Kadena bridge", () => {
+  test.todo(
+    "This is an empty test to make jest command pass. Remove it once there is a real test.",
+  );
+});
+
+
 export const dataset: DatasetTest<Transaction> = {
   implementations: ["js"],
   currencies: {
     kadena,
   },
 };
-
-describe("Kadena bridge", () => {
-  test.todo(
-    "This is an empty test to make jest command pass. Remove it once there is a real test.",
-  );
-});
