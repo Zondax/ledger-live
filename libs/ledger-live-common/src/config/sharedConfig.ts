5b4f4c2c52382f92a0ce53840c1251a3881892c7
Maria Ayelen Murano <muranoayelen@gmail.com>
2024-08-27 11:52:16 -0300

    feat(kda): get api endpoint from config

--- libs/ledger-live-common/src/config/sharedConfig.ts
+++ libs/ledger-live-common/src/config/sharedConfig.ts
@@ -11,10 +11,11 @@ import { elrondConfig } from "../families/elrond/config";
 import { evmConfig } from "../families/evm/config";
 import { fileCoinConfig } from "../families/filecoin/config";
 import { hederaConfig } from "../families/hedera/config";
+import { iconConfig } from "../families/icon/config";
 import { internetComputerConfig } from "../families/internet_computer/config";
+import { kadenaConfig } from "../families/kadena/config";
 import { nearConfig } from "../families/near/config";
 import { polkadotConfig } from "../families/polkadot/config";
-import { xrpConfig } from "../families/xrp/config";
 import { solanaConfig } from "../families/solana/config";
 import { stacksConfig } from "../families/stacks/config";
 import { stellarConfig } from "../families/stellar/config";
@@ -22,7 +23,7 @@ import { tezosConfig } from "../families/tezos/config";
 import { tonConfig } from "../families/ton/config";
 import { tronConfig } from "../families/tron/config";
 import { vechainConfig } from "../families/vechain/config";
-import { iconConfig } from "../families/icon/config";
+import { xrpConfig } from "../families/xrp/config";
 
 const countervaluesConfig: ConfigSchema = {
   config_countervalues_refreshRate: {
@@ -65,4 +66,5 @@ export const liveConfig: ConfigSchema = {
   ...vechainConfig,
   ...iconConfig,
   ...tonConfig,
+  ...kadenaConfig,
 };
