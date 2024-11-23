69ff3e461c73c450e87698a8f725f55676fff623
Prateek Rathod <emaillawrathod@gmail.com>
2024-11-23 17:29:30 +0530

    feat(icp): add incremental sync

--- libs/ledger-live-common/src/families/internet_computer/consts.ts
+++ libs/ledger-live-common/src/families/internet_computer/consts.ts
@@ -6,7 +6,11 @@ export const ICP_LIST_NEURONS_TXN_TYPE = 1;
 // ICP Canister Ids
 export const MAINNET_LEDGER_CANISTER_ID = "ryjl3-tyaaa-aaaaa-aaaba-cai";
 export const MAINNET_GOVERNANCE_CANISTER_ID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
+export const MAINNET_INDEX_CANISTER_ID = "q3fc5-haaaa-aaaaa-aaahq-cai";
 export const DEFAULT_INGRESS_EXPIRY_DELTA_IN_MSECS = 5 * 60 * 1000;
+
+// API limits
+export const FETCH_TXNS_LIMIT = 100;
 
 // Max ICP fees
 export const ICP_FEES = 1e4;
